2acc7e64e7c5bcf750b9a249db22fcd8b274da06
gmsolem <geir.solem.m@gmail.com>
2018-09-20 13:53:47 -0500

    Update and rename TestBAE.sol to Testbae.sol

--- test/Testbae.sol
+++ test/Testbae.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.4.2;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/Radex.sol";
+import "../contracts/Bae.sol";
 
 contract TestBae {
   function testTokenAddress() {
-    Radex rdx = Bae(DeployedAddresses.Radex());
+    Bae rdx = Bae(DeployedAddresses.Bae());
     uint256 feeMultiplier = rdx.feeMultiplier();
 
     Assert.equal(feeMultiplier, 1000, "Bae charges market takers 0.1% fee and gives it to market makers.");
