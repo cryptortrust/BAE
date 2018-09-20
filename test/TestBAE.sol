d129fe098fe04564b4224a41657d5c3b4d0113c5
gmsolem <geir.solem.m@gmail.com>
2018-09-20 13:18:02 -0500

    Update and rename TestRadex.sol to TestBAE.sol

--- test/TestBAE.sol
+++ test/TestBAE.sol
@@ -4,11 +4,11 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Radex.sol";
 
-contract TestRadex {
+contract TestBae {
   function testTokenAddress() {
-    Radex rdx = Radex(DeployedAddresses.Radex());
+    Radex rdx = Bae(DeployedAddresses.Radex());
     uint256 feeMultiplier = rdx.feeMultiplier();
 
-    Assert.equal(feeMultiplier, 1000, "Radex charges market takers 0.1% fee and gives it to market makers.");
+    Assert.equal(feeMultiplier, 1000, "Bae charges market takers 0.1% fee and gives it to market makers.");
   }
 }
