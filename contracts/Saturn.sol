c8d8e0a69e64128cadc9726c5870b69c58a5b30b
gmsolem <geir.solem.m@gmail.com>
2018-09-20 12:55:17 -0500

    Update Saturn.sol

--- contracts/Saturn.sol
+++ contracts/Saturn.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.11;
 
 import "./ERC223.sol";
 
-contract Saturn is ERC223Token {
-  string public name = "Saturn";
+contract Baenet is ERC223Token {
+  string public name = "Baenet";
   string public symbol = "STN";
   uint public decimals = 4;
   uint public INITIAL_SUPPLY = 2000000000;
 
-  function Saturn() {
+  function () {Baenet
     totalSupply = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
   }
